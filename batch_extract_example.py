e3e5854cb3c612d25eece0a2efc431c4d8718b52
Tuomas Karna <tuomas.karna@gmail.com>
2018-11-21 17:42:55 +0200

    update batch extract example

--- batch_extract_example.py
+++ batch_extract_example.py
@@ -5,12 +5,14 @@ from siri_omen import *
 
 run_id = 'nemo4-run013'
 
-obs_search_pattern = 'obs/ts_*ssh*.nc'
 source_file_pattern = 'NORDIC_1h_20180901_20181029_SURF_grid_T_2018091*.nc'
+obs_search_pattern = 'obs/ts_*ssh*.nc'
+extract_timeseries_from_obs(run_id, source_file_pattern, obs_search_pattern)
 
-#extract_timeseries_from_obs(run_id, source_file_pattern, obs_search_pattern)
 
-obs_search_pattern = 'obs/ts_*salt*.nc'
 source_file_pattern = 'NORDIC_1h_20180901_20181029_grid_T_2018091*.nc'
+obs_search_pattern = 'obs/ts_*salt*.nc'
+extract_timeseries_from_obs(run_id, source_file_pattern, obs_search_pattern)
 
+obs_search_pattern = 'obs/ts_*temp*.nc'
 extract_timeseries_from_obs(run_id, source_file_pattern, obs_search_pattern)
